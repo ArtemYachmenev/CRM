72b53b924db115d58dcc8bab6530aa2457e7f24c
Ячменев Артём <iachmeneow.a@yandex.ru>
2024-02-09 17:23:13 +0500

    подготовил файлы для начала работы проекта, испрвить ошибки при вызове команды npx gulp при вызове в "C:\Users\Ячменев Артём\IdeaProjects\CRMUltimateINT\fronted\source"

---
+++ fronted/webpack.config.js
@@ -0,0 +1,17 @@
+//конфигурация собирающая наш бандл
+config = require('./config.js');
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './' + config.dir.source.js + 'app.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.js/,
+            use: 'import-glob'
+        }]
+    }
+};
